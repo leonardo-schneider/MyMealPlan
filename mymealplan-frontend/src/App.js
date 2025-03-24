8c803d773a92bdaa2ddd195448909260271f6365
Leonardo Schneider <leonardoluis5@hotmail.com>
2025-03-24 12:33:06 -0500

    Created home and profile urls

--- mymealplan-frontend/src/App.js
+++ mymealplan-frontend/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import your page components
+import Home from './pages/Home';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
-import MealPlanSelection from './pages/MealPlanSelection';
 import NotFoundPage from './pages/NotFoundPage';
+import Profile from './pages/Profile';
 
 function App() {
   return (
